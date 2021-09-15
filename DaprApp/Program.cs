eabf04f4f5f2219d00a9d3a9a93f5be11f11d97c
Manuel Riezebosch <mriezebosch@gmail.com>
2021-09-15 13:52:07 +0200

    refactor(program): exclude from coverage

--- DaprApp/Program.cs
+++ DaprApp/Program.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace DaprApp
 {
+    [ExcludeFromCodeCoverage]
     public class Program
     {
         public static void Main(string[] args)
