066ea3a63b6027b4523e4dc1393f95ea0e9e86b8
Martijn Tieland <mtieland@xpirit.com>
2022-10-26 19:23:59 +0200

    More tests

---
+++ Rebus/CloudEventify.Rebus.Tests/CloudEventExtensionTests.cs
@@ -0,0 +1,93 @@
+﻿using AutoFixture;
+using CloudNative.CloudEvents;
+using FluentAssertions;
+using Rebus.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static CloudEventify.Rebus.Tests.WrapTests;
+
+namespace CloudEventify.Rebus.Tests;
+
+public class CloudEventExtensionTests
+{
+
+    private Dictionary<string,string> GetHeaders()
+    {
+        return new Dictionary<string, string> {
+                { Headers.ContentEncoding, "UTF-8" },
+                { Headers.ContentType, "application/json" },
+                { Headers.CorrelationId, Guid.NewGuid().ToString() },
+                { Headers.CorrelationSequence, Guid.NewGuid().ToString() },
+                { Headers.DeferCount, "3" },
+                { Headers.DeferredRecipient, "abc" },
+                { Headers.DeferredUntil, "123" },
+                { Headers.ErrorDetails, "456" },
+                { Headers.Express, "789" },
+                { Headers.InReplyTo, "sb://something.uri" },
+                { Headers.Intent, "p2p" },
+                { Headers.MessageId, Guid.NewGuid().ToString() },
+                { Headers.MessagePayloadAttachmentId, "45672" },
+                { Headers.ReturnAddress, "sb:/replyhere.com" },
+                { Headers.RoutingSlipItinerary, "23" },
+                { Headers.RoutingSlipTravelogue, "0987654" },
+                { Headers.SenderAddress, "1234" },
+                { Headers.SourceQueue, "gbfd34" },
+                { Headers.TimeToBeReceived, new DateTimeOffset(2007, 11, 1, 21, 0, 0, new TimeSpan(-2, 0, 0)).ToString("O") },
+                { Headers.Type, "SomeDataObject" },
+                { Headers.SentTime, new DateTimeOffset(2007, 10, 31, 21, 0, 0, new TimeSpan(-8, 0, 0)).ToString("O") }
+        };
+    }
+
+    private Message GetMessage(Dictionary<string,string> headers)
+    {
+        var fact = SomeDataObject.GetRandom();
+        return new Message(headers, fact);
+    }
+
+    private CloudEvent GetCloudEvent(Message msg)
+    {
+        var sut = new Wrap(new Mapper().Map<SomeDataObject>("myDataObjectTopic"), new Uri("test:uri"));
+        var res = sut.Envelope(msg);
+        return res;
+    }
+
+    [Fact]
+    public void RebusHeaders_From_CloudEvent_Should_Contain_all_Rbs_headers()
+    {
+        var factheaders = GetHeaders();
+        var factMsg = GetMessage(factheaders);
+        var cloudEvent = GetCloudEvent(factMsg);
+
+        Assert.NotNull(cloudEvent);
+        cloudEvent.Id.Should().Be(factheaders[Headers.MessageId]);
+        
+        var headers = cloudEvent.GetRebusHeaders();
+        headers.Should().BeEquivalentTo(factheaders);
+        headers.Count.Should().Be(21);
+    }
+
+    [Fact]
+    public void RebusHeaders_From_CloudEvent_Should_Work_without_custom_headers()
+    {
+        var factheaders = new Dictionary<string, string>
+        {
+            { Headers.MessageId, Guid.NewGuid().ToString()}
+        };
+        var factMsg = GetMessage(factheaders);
+        var cloudEvent = GetCloudEvent(factMsg);    
+
+
+        Assert.NotNull(cloudEvent);
+        cloudEvent.Id.Should().Be(factheaders[Headers.MessageId]);
+
+        var headers = cloudEvent.GetRebusHeaders();
+        headers.Should().BeEquivalentTo(factheaders);
+        headers[Headers.MessageId].Should().Be(factheaders[Headers.MessageId]);
+        headers.Count.Should().Be(1);
+
+    }
+}
---
+++ Rebus/CloudEventify.Rebus.Tests/HeaderMapTests.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using Rebus.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CloudEventify.Rebus.Tests
+{
+    public class HeaderMapTests
+    {
+        [Fact]
+        public void Should_Contain_All_Known_Rebus_Headers()
+        {
+            var map = HeaderMap.Instance;
+
+            Assert.NotNull(map);
+
+            map.Forward.Keys.Should().Contain(Headers.ContentEncoding);
+            map.Forward.Keys.Should().Contain(Headers.ContentType);
+            map.Forward.Keys.Should().Contain(Headers.CorrelationId);
+            map.Forward.Keys.Should().Contain(Headers.CorrelationSequence);
+            map.Forward.Keys.Should().Contain(Headers.DeferCount);
+            map.Forward.Keys.Should().Contain(Headers.DeferredRecipient);
+            map.Forward.Keys.Should().Contain(Headers.DeferredUntil);
+            map.Forward.Keys.Should().Contain(Headers.ErrorDetails);
+            map.Forward.Keys.Should().Contain(Headers.Express);
+            map.Forward.Keys.Should().Contain(Headers.InReplyTo);
+            map.Forward.Keys.Should().Contain(Headers.Intent);
+            map.Forward.Keys.Should().Contain(Headers.MessageId);
+            map.Forward.Keys.Should().Contain(Headers.MessagePayloadAttachmentId);
+            map.Forward.Keys.Should().Contain(Headers.ReturnAddress);
+            map.Forward.Keys.Should().Contain(Headers.RoutingSlipItinerary);
+            map.Forward.Keys.Should().Contain(Headers.RoutingSlipTravelogue);
+            map.Forward.Keys.Should().Contain(Headers.SenderAddress);
+            map.Forward.Keys.Should().Contain(Headers.SentTime);
+            map.Forward.Keys.Should().Contain(Headers.SourceQueue);
+            map.Forward.Keys.Should().Contain(Headers.TimeToBeReceived);
+            map.Forward.Keys.Should().Contain(Headers.Type);
+
+            map.Forward.Keys.Distinct().Count().Should().Be(21);
+            map.Forward.Values.Distinct().Count().Should().Be(21);
+
+            map.Forward[Headers.ContentEncoding].Should().Be("r2contentencoding");
+            map.Forward[Headers.ContentType].Should().Be("r2contenttype");
+            map.Forward[Headers.CorrelationId].Should().Be("r2corrid");
+            map.Forward[Headers.CorrelationSequence].Should().Be("r2corrseq");
+            map.Forward[Headers.DeferCount].Should().Be("r2defercount");
+            map.Forward[Headers.DeferredRecipient].Should().Be("r2deferredrecipient");
+            map.Forward[Headers.DeferredUntil].Should().Be("r2deferreduntil");
+            map.Forward[Headers.ErrorDetails].Should().Be("r2errordetails");
+            map.Forward[Headers.Express].Should().Be("r2express");
+            map.Forward[Headers.InReplyTo].Should().Be("r2inreplyto");
+            map.Forward[Headers.Intent].Should().Be("r2intent");
+            map.Forward[Headers.MessageId].Should().Be("r2msgid");
+            map.Forward[Headers.MessagePayloadAttachmentId].Should().Be("r2msgattachementid");
+            map.Forward[Headers.ReturnAddress].Should().Be("r2returnaddress");
+            map.Forward[Headers.RoutingSlipItinerary].Should().Be("r2routingitinerary");
+            map.Forward[Headers.RoutingSlipTravelogue].Should().Be("r2routingtravelogue");
+            map.Forward[Headers.SenderAddress].Should().Be("r2senderaddress");
+            map.Forward[Headers.SentTime].Should().Be("r2senttime");
+            map.Forward[Headers.SourceQueue].Should().Be("r2sourcequeue");
+            map.Forward[Headers.TimeToBeReceived].Should().Be("r2timetobereceived");
+            map.Forward[Headers.Type].Should().Be("r2msgtype");
+        }
+
+        [Fact]
+        public void Should_be_Singleton()
+        {
+            var map = HeaderMap.Instance;
+            var map2 = HeaderMap.Instance;
+            HeaderMap.ReferenceEquals(map, map2);
+            map2.Should().BeEquivalentTo(map);
+        }
+    }
+}
