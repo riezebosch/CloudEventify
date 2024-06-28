9e257590d73d5e1211090cc2086ebf6193c0d789
Manuel Riezebosch <mriezebosch@gmail.com>
2024-06-28 11:09:55 +0200

    refactor(cloudeventify): use polysharp for new C# features

--- CloudEventify/IsExternalInit.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel;
-
-// https://stackoverflow.com/a/62656145/129269
-namespace System.Runtime.CompilerServices
-{
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    internal class IsExternalInit{}
-}
